b7d153ed56057ac8b384985b81c69f8e9b9726d9
Pushkarkumarlal <pushkarkumarlal@gmail.com>
2023-07-11 20:14:21 +0530

    name chnaged

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello Hello Bandri</Text>
+      <Text>Hello Hello pushkar Bandri</Text>
       <StatusBar style="auto" />
     </View>
   );
