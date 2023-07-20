5e483295def163433f80cbdc27e407a363d7701c
badreerauniyar <badree.rauniyar@gmail.com>
2023-07-21 01:01:09 +0530

    login page responsive

--- app/login/loginpage.js
+++ app/login/loginpage.js
@@ -14,28 +14,41 @@ const loginpage=()=>{
 
     return(
             <SafeAreaView style={styles.container}>
-                <Image source={topimage} style={styles.topDesignRed}/>
-
-                <View style={styles.inputContainer}>
-                <TextInput placeholder='User Name' style={styles.input} onChangeText={setUserName} value={userName}/>
-                <TextInput secureTextEntry={true} placeholder='Password' style={styles.input} onChangeText={setPassword} value={password}/>
+                <View style={styles.container1}>
+                    <Image source={topimage} style={styles.imageStyle}/>
                 </View>
+                <View style={styles.container2}>
+                    <View style={styles.userNameContainer}>
+                        <View style={styles.whiteBox}>
+                            <TextInput style={styles.inputText} placeholder='User Name' onChangeText={setUserName} value={userName}/>
+                        </View>
+                    </View>
 
-                <TouchableOpacity style={styles.loginButton}>
-                <Text style={styles.loginButtonText}><Link href='../dashboard/dashboard'>LOGIN</Link></Text>
-                </TouchableOpacity>
-
-                <View style={styles.buttomViewContainer}>
-
-                <TouchableOpacity style={styles.touchRegister}>
-                <Text style={styles.register}><Link href='../createaccount/register'>Register</Link></Text>
-                </TouchableOpacity>
-
-                <TouchableOpacity style={styles.touchForgetPassword}>
-                <Text style={styles.forgetPassword} ><Link href ='../forgetPassword/forgetpassword'>Forget Password?</Link></Text>
-                </TouchableOpacity>
+                    <View style={styles.userNameContainer}>
+                        <View style={styles.whiteBox}>
+                        <TextInput style={styles.inputText} secureTextEntry={true} placeholder='Password' onChangeText={setPassword} value={password}/>
+                        </View>
+                    </View>
                 </View>
-
+                <View style={styles.container3}>
+                    <View style={styles.LoginContainer}>
+                        <TouchableOpacity>
+                            <View style={styles.loginButton}>
+                                <Text style={styles.loginButtonText}><Link href='../dashboard/dashboard'>LOGIN</Link></Text>
+                            </View>
+                        </TouchableOpacity>
+                    </View>
+                    <View style={styles.registerContainer}>
+                        <TouchableOpacity>
+                            <Text style={styles.register}><Link href='../createaccount/register'>Register</Link></Text>
+                            <View style={styles.touchRegister}></View>
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                            <Text style={styles.forgetPassword} ><Link href ='../forgetPassword/forgetpassword'>Forget Password?</Link></Text>
+                            <View style={styles.touchForgetPassword}></View>
+                        </TouchableOpacity>
+                    </View>
+                </View>
             </SafeAreaView>
     )
 }
