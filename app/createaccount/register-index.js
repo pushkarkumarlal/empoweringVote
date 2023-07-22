8d36421a79d55051ecfe693d38e434136aebdb12
Pushkarkumarlal <pushkarkumarlal@gmail.com>
2023-07-22 17:32:54 +0530

    Phone-Num and Otp Verification UI

---
+++ app/createaccount/register-index.js
@@ -0,0 +1,11 @@
+import react from "react";
+
+import PhoneNumber from "./phone-num-register-page/phone-num";
+
+const Register=()=>{return(
+    <>
+    <PhoneNumber/>
+    </>
+)}
+
+export default Register
